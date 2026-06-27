e43ed204ec28154e97f72d7214674abea80466cb
Ahmad Fawad Amiri <ahmadfawad.amiri1@gmail.com>
2026-06-27 18:03:33 +0200

    opimize exception handler

--- src/main/java/com/project/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/ecommerce/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.project.ecommerce.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex, HttpServletRequest request){
@@ -30,19 +34,14 @@ public class GlobalExceptionHandler {
                 request.getRequestURI(),
                 errors
         );
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("status", HttpStatus.BAD_REQUEST.value());
-//        body.put("error", "Validation Error");
-//        body.put("errors", errors);
-//        body.put("path", request.getRequestURI());
-
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException ex, HttpServletRequest request){
+
+        log.warn("Entity not found: {}", ex.getMessage());
+        log.debug("Stack trace", ex);
 
         ErrorResponse response = new ErrorResponse(
                 LocalDateTime.now(),
@@ -52,15 +51,6 @@ public class GlobalExceptionHandler {
                 request.getRequestURI(),
                 null
         );
-    //public ResponseEntity<Map<String, Object>> handleEntityNotFound(EntityNotFoundException ex, HttpServletRequest request){
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("status", HttpStatus.NOT_FOUND.value());
-//        body.put("erorr", "Not found");
-//        body.put("message", ex.getMessage());
-//        body.put("path", request.getRequestURI());
-
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
@@ -70,20 +60,11 @@ public class GlobalExceptionHandler {
         ErrorResponse response = new ErrorResponse(
                 LocalDateTime.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "INTERNAL_SERVER_ERROR",
+                "INTERNAL_SERVER_OCCURRED",
                 ex.getMessage(),
                 request.getRequestURI(),
                 null
         );
-//    public ResponseEntity<Map<String, Object>> handleGeneral(Exception ex, HttpServletRequest request){
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-//        body.put("error", "Internal Server Error");
-//        body.put("message", ex.getMessage());
-//        body.put("path", request.getRequestURI());
-
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
